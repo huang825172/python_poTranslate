4fd87040691adf0e74c5dcb9bfa03c327fa7bdf8
huang825172 <1048035187@qq.com>
2019-10-24 23:46:55 +0800

    Tested to totally copy the .po file after restructing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from potranslator import translator
 
 if __name__ == "__main__":
-    translator.translate("wedevs-project-manager-zh_CN.po")
+    # translator.translate("wedevs-project-manager-zh_CN.po")
+    translator.translate("ultimate-member-zh_CN.po")
--- potranslator/translator.py
+++ potranslator/translator.py
@@ -11,7 +11,7 @@ def translate(filename):
         po_reader = libpo_reader.Instance(filename)
         po_filter = libpo_filter.Instance()
         po_connector = libpo_connector.Instance()
-        po_writer = open("out.po", 'w')
+        po_writer = open("out.po", 'w', encoding='UTF-8')
         for part in po_reader.get_filestruct():
             if part[0] == META_LABEL:
                 po_writer.write(part[1])
@@ -22,13 +22,16 @@ def translate(filename):
                         po_writer.write('"{}"\n'.format(part[1][idx]))
                     po_writer.write('msgstr "{}"\n'.format(part[1][-1]))
                 else:
+                    po_writer.write("msgid ")
                     for idx in range(len(part[1]) - 1):
                         po_filter.init(part[1][idx])
                         po_filter.process()
                         po_connector.init(po_filter.get_pack())
-                        po_writer.write(str(po_connector.get_not_translated()))
+                        po_writer.write('"{}"\n'.format(part[1][idx]))
+                        # po_writer.write(str(po_connector.get_not_translated()))
                         # po_connector.print_raw()
                         # print(po_connector.set_translated(["任务标记为", "在里面"]))
+                    po_writer.write('msgstr ""\n')
         po_writer.close()
     except FileNotFoundError as e:
         print(e)
